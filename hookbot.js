370a94b9d6a056c96f1f916db702a80ddde5956d
jimmyy4 <jimmy@jpmiii.com>
2018-10-10 18:30:15 -0500

    slightly better this time

--- hookbot.js
+++ hookbot.js
@@ -1,17 +1,17 @@
 const request = require('request-promise-native');
 var webhooks = require('./webhooks.json');
-var baseTags = '';
+var config = require('./config.json');
 var recentID;
 var previousID;
 
-request(`https://danbooru.donmai.us/posts.json${baseTags ? `?tags=${baseTags}&` : '?'}limit=1`,{json: true}).then((posts)=>{
+request(`https://danbooru.donmai.us/posts.json${config.baseTags ? `?tags=${config.baseTags}&` : '?'}limit=1`,{json: true}).then((posts)=>{
     recentID = posts[0].id;
     console.log(`Starting at ${recentID}`)
 }).catch(console.error);
 
 setInterval(()=>{
     console.log(`Checking Danbooru starting at ${recentID}`)
-    request(`https://danbooru.donmai.us/posts.json?tags=${baseTags ? baseTags+'+' : ''}id:>=${recentID}`,{json: true}).then((posts) => {
+    request(`https://danbooru.donmai.us/posts.json?tags=${config.baseTags ? config.baseTags+'+' : ''}id:>=${recentID}`,{json: true}).then((posts) => {
         recentID = posts[0].id;
         if(previousID === recentID) posts.splice(0,1);
         previousID = recentID;
@@ -19,14 +19,18 @@ setInterval(()=>{
             webhooks.forEach(hook => {
                 if(hook.tags.every(tag => {return post.tag_string.includes(tag)}) && hook.safe ? post.rating === 's' : post.rating != 's'){
                     console.log(`${post.id} matches ${hook.tags} safe: ${hook.safe}`)
+                    let color = post.id-(Math.floor(post.id/16777215)*16777215);
                     let options = {
                         method: 'POST',
                         uri: `https://canary.discordapp.com/api/webhooks/${hook.uri}`,
                         body: {
                             embeds: [{
-                                title: `New post matching ${hook.tags} ${hook.safe ? '' : '-'}rating:s`,
+                                title: `${post.tag_string_character} by ${post.tag_string_artist} (matching ${hook.tags} ${hook.safe ? '' : '-'}rating:s)`,
                                 url: `https://danbooru.donmai.us/posts/${post.id}`,
-                                image: {url:post.file_url}
+                                image: {url:post.file_url},
+                                timestamp: post.created_at,
+                                color: color,
+                                description: `[Large](${post.source})`
                             }]
                         },
                         json: true
